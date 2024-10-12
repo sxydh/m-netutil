40aaa5ece954bbb12014a2412263fda7292cd338
sxydh <sxydhgg@gmail.com>
2024-10-12 20:49:13 +0800

    [UPDATE]

---
+++ MNetUtil.Test/Core/FileServerTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MNetUtil.Test.Core
+{
+    [TestClass]
+    public class FileServerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
